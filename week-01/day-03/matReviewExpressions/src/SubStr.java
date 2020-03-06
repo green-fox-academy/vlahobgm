8339f7d3cfb3c2c6b79240d7c50d7f45f5cc3da9
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-06 17:24:44 +0100

    corrected SubStr

--- week-01/day-03/matReviewExpressions/src/SubStr.java
+++ week-01/day-03/matReviewExpressions/src/SubStr.java
@@ -1,23 +1,5 @@
-import org.w3c.dom.ls.LSOutput;
-
-import java.sql.SQLOutput;
-
 public class SubStr {
     public static void main(String[] args) {
-        String str = "Hello";
-        String str2 = "ll";
-        System.out.println(str.length());
-        System.out.println(str.contains(str2));
-        System.out.println(str2.length());
-
-        for (int i = 0; i < str.length() ; i++) {
-            
-        }
-
-    }
-
-
-    /*public static void main(String[] args) {
         //  Create a function that takes two strings as a parameter
         //  Returns the starting index where the second one is starting in the first one
         //  Returns `-1` if the second string is not in the first one
@@ -28,12 +10,18 @@ public class SubStr {
         System.out.println(subStr("this is what I'm searching in", "searching"));
 
         //  should print: `-1`
-        System.out.println(subStr("this is what I'm searching in", "not"));
+       System.out.println(subStr("this is what I'm searching in", "not"));
 
     }
 
     public static int subStr(String input, String q) {
-
-        return -1;
-    }*/
+        /*int position = input.indexOf(q);
+        if (position >= 0){
+            return position;
+        }
+        else {
+            return -1;
+        }*/
+        return input.indexOf(q);  //if not contain, -1 will be the result, search Q position in INPUT
+    }
 }

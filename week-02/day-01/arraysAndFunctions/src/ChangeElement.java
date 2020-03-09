84c1e99d4c67807225c0636264f30a04d173bc76
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-09 14:27:43 +0100

    ChangeElement fixed

--- week-02/day-01/arraysAndFunctions/src/ChangeElement.java
+++ week-02/day-01/arraysAndFunctions/src/ChangeElement.java
@@ -1,7 +1,13 @@
 public class ChangeElement {
     public static void main(String[] args) {
         int [] numbers = {1, 2, 3, 8, 5, 6};
-        numbers[3] = 4;
+        /*numbers[3] = 4;
+        System.out.println(numbers[3]);*/
+        for (int i = 0; i < numbers.length ; i++) {
+            if (numbers[i] == 8){
+                numbers[i] = 4;
+            }
+        }
         System.out.println(numbers[3]);
     }
 }

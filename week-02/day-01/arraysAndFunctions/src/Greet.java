93cd28d1edfa5583aac3ff2edc241d0504614f2a
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-09 15:00:31 +0100

    greet added

---
+++ week-02/day-01/arraysAndFunctions/src/Greet.java
@@ -0,0 +1,12 @@
+public class Greet {
+    public static void main(String[] args) {
+        String al = "Green Fox";
+
+        System.out.println(greet(al) + al);
+    }
+
+    public static String greet(String gr) {
+        gr = "Greetings dear ";
+        return gr;
+    }
+}

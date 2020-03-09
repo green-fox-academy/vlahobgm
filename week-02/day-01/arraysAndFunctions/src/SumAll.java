78b89bcf3aeae9d908754e155c354a717aae4c50
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-09 14:18:31 +0100

    SumAll added

---
+++ week-02/day-01/arraysAndFunctions/src/SumAll.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+
+public class SumAll {
+    public static void main(String[] args) {
+        int [] numbers = {3, 4, 5, 6, 7};
+        int sum = 0;
+        for (int i = 0; i < numbers.length ; i++) {
+            sum += numbers[i];
+        }
+        System.out.println(sum);
+    }
+}

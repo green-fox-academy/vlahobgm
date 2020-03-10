31ac1d572b7c642b9abce0753ddf81866c6948de
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-10 16:21:44 +0100

    MapIntro2 corrected

--- week-02/day-02/DataStructures/src/MapIntroduction2.java
+++ week-02/day-02/DataStructures/src/MapIntroduction2.java
@@ -16,6 +16,11 @@ public class MapIntroduction2 {
         map.remove("978-1-60309-444-3");
 
         //Remove the key-value pair with value The Lab
+        for (Map.Entry<String, String> item : map.entrySet()) {
+            if (item.getValue() == "The Lab") {
+                map.remove(item.getKey());
+            }
+        }
 
         map.put("978-1-60309-450-4", "They Called Us Enemy");
         map.put("978-1-60309-453-5", "Why Did We Trust Him?");

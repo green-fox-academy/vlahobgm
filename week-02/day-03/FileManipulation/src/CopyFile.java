25eedbe8ed53301cf1ca0885945e9016e8425fb0
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-11 15:51:39 +0100

    CopyFile fixed

--- week-02/day-03/FileManipulation/src/CopyFile.java
+++ week-02/day-03/FileManipulation/src/CopyFile.java
@@ -1,8 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CopyFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Write a function that copies the contents of a file into another
         // It should take the filenames as parameters
         // It should return a boolean that shows if the copy was successful
+        copyContent(Paths.get("copyFilesource.txt"), Paths.get("copyFileDestination.txt"));
+    }
+    public static boolean copyContent(Path source, Path destination) throws IOException {
+        List<String> content = new ArrayList<>();
+        List<String> sourceLines = Files.readAllLines(source);
+        for (int i = 0; i < sourceLines.size(); i++) {
+            content.add(sourceLines.get(i));
+        }
+        try {
+            Files.write(destination,content);
+            return true;
+        }catch (IOException e){
+            return false;
+        }
+
 
     }
 }

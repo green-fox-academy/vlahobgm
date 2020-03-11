b8327039780fae0398f0ac06109c05873ff38a98
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-11 15:11:57 +0100

    ReversedList added

---
+++ week-02/day-03/FileManipulation/src/ReversedLines.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReversedLines {
+    public static void main(String[] args) {
+        // Create a method that decrypts reversed-lines.txt
+        try{
+            reversed();
+        }catch (IOException e){
+            System.out.println("Something went wrong");
+        }
+    }
+
+    public static void reversed() throws IOException {
+        Path filePath = Paths.get("reversed-lines.txt");
+        List<String> lines = Files.readAllLines(filePath);
+        List<String> newLines = new ArrayList<>();
+        for (String line : lines) {
+            String newline = "";
+            for (int j = line.length() - 1; j >= 0; j--) {
+                newline += line.charAt(j);
+            }
+            newLines.add(newline);
+        }
+        Files.write(filePath,newLines);
+    }
+}

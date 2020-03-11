01bdd3e99d109a2c26e55429b503880653ee43cb
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-11 12:02:11 +0100

    Writesingleline and myfiletxt added

---
+++ week-02/day-03/FileManipulation/src/WriteSingleLine.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteSingleLine {
+    public static void main(String[] args) {
+        // Write a function that is able to manipulate a file
+        // By writing your name into it as a single line
+        // The file should be named "my-file.txt"
+        // In case the program is unable to write the file,
+        // It should print the following error message: "Unable to write file: my-file.txt"
+        try {
+            writeFile();
+        }catch (IOException e){
+            System.out.println("Unable to write file : my-file.txt");
+        }
+
+    }
+    public static void writeFile() throws IOException {
+        Path filePath = Paths.get("my-file.txt");
+        List<String> content = new ArrayList<>();
+        content.add("Béci");
+        Files.write(filePath,content);
+
+    }
+}

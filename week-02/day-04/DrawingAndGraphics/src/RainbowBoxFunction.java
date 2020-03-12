4c453b0e64e0c26674c435d03f6f942dad4ba917
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-12 13:34:17 +0100

    RainbowBoxFunction added

---
+++ week-02/day-04/DrawingAndGraphics/src/RainbowBoxFunction.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class RainbowBoxFunction {
+    public static void mainDraw(Graphics graphics) {
+        // Create a square drawing function that takes 3 parameters:
+        // The square size, the fill color, graphics
+        // and draws a square of that size and color to the center of the canvas.
+        // Create a loop that fills the canvas with rainbow colored squares (red, orange, yellow, green, blue, indigo, violet).
+        Color[] colorArray = new Color[]{Color.RED,Color.ORANGE,Color.YELLOW,Color.GREEN,Color.BLUE,new Color(75,0,130),new Color(238,130,238),};
+        //violet (238,130,238)
+        //indigo (75,0,130)
+
+        for (int i = colorArray.length-1; i >= 0; i--) {
+            drawQuare(40*i+40,colorArray[i],graphics);
+        }
+
+    }
+    public static void drawQuare(int size, Color color, Graphics graphics){
+        graphics.setColor(color);
+        graphics.fillRect(WIDTH/2-size/2, HEIGHT/2 - size/2, size, size);
+
+        
+    }
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}

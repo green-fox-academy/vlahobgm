4f2f92d48ae7fa842ff4d66dc783027c1f067c3f
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-17 15:25:11 +0100

    counter added

---
+++ week-03/day-02/src/counter/Counter.java
@@ -0,0 +1,28 @@
+package counter;
+
+public class Counter {
+  Integer current = 0;
+  Integer defaultVal = 0;
+
+  public Counter(int current){
+    this.current = current;
+  }
+  public Counter(){
+  }
+
+  public void add(int number){
+    this.current += number;
+  }
+
+  public void add(){
+    this.current += 1;
+  }
+
+  public int get(){
+    return current;
+  }
+
+  public void reset(){
+    this.current = defaultVal;
+  }
+}

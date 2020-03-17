8992571183c660b700c0232908d4dcc64714406b
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-17 14:49:15 +0100

    animal added

---
+++ week-03/day-02/src/animal/Main.java
@@ -0,0 +1,13 @@
+package animal;
+
+public class Main {
+  public static void main(String[] args) {
+    Animal dog = new Animal();
+    Animal cat = new Animal();
+    dog.play();
+    dog.drink();
+    cat.eat();
+
+    System.out.println();
+  }
+}

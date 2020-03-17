6dc07ba25f3095eb9970224ab375c34349083b31
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-17 14:08:50 +0100

    Post it added

---
+++ week-03/day-02/src/postIt/Main.java
@@ -0,0 +1,9 @@
+package postIt;
+
+import java.awt.Color;
+
+public class Main {
+  PostIt post1 = new PostIt(Color.ORANGE,"Idea 1",Color.BLUE);
+  PostIt post2 = new PostIt(Color.PINK, "Awesome",Color.BLACK);
+  PostIt post3 = new PostIt(Color.YELLOW,"Superb!",Color.GREEN);
+}
---
+++ week-03/day-02/src/postIt/PostIt.java
@@ -0,0 +1,15 @@
+package postIt;
+
+import java.awt.Color;
+
+public class PostIt {
+  Color background;
+  String text;
+  Color textColor;
+
+  public PostIt(Color background, String text, Color textColor){
+    this.background = background;
+    this.text = text;
+    this.textColor = textColor;
+  }
+}

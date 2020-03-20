e0f46a7ddab35f2101004f5f33a2c1f47791b8e9
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-20 11:44:33 +0100

    fibonacci updated

--- week-03/day-04/test/fibonacci/FibonacciTest.java
+++ week-03/day-04/test/fibonacci/FibonacciTest.java
@@ -9,11 +9,12 @@ public class FibonacciTest {
 
   @Test
   public void calcMemberFibonacci() {
+    //Setup
     Fibonacci fibo = new Fibonacci();
-
+    //Act
     int index = 6;
     int valueIndex = fibo.calcMemberFibonacci(index);
-    
+    //Assertion
     assertEquals(8,valueIndex);
   }
 

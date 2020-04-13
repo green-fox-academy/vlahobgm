d55b2b5705949434c1ee7aa6b6dc7743c5e9c2a5
Bela Vlahovics <vlahobgm@gmail.com>
2020-04-13 11:11:17 +0200

    first exercise corrected

--- week-06/day-01/src/exercises/ExerciseOne.java
+++ week-06/day-01/src/exercises/ExerciseOne.java
@@ -7,6 +7,6 @@ public class ExerciseOne {
   public static void main(String[] args) {
     //Write a Stream Expression to get the even numbers from the following list:
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-    numbers.stream().filter(x -> x % 2 == 1 || x % 2 == -1).forEach(System.out::println);
+    numbers.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
   }
 }

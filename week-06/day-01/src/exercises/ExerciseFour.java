8953f17d6536977c8ecdbdc730da628a0613421f
Bela Vlahovics <vlahobgm@gmail.com>
2020-04-13 13:54:58 +0200

    fourth exercise added

---
+++ week-06/day-01/src/exercises/ExerciseFour.java
@@ -0,0 +1,18 @@
+package exercises;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class ExerciseFour {
+  public static void main(String[] args) {
+    //Write a Stream Expression to get the average value of the odd numbers from the following list:
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    OptionalDouble average = numbers.stream()
+        .filter(x -> x > 0)
+        .mapToInt((x) -> x)
+        .average();
+    System.out.println(average);
+
+  }
+}

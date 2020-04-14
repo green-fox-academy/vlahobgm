92e16a1575eec19529aa90e1bb3ab9019c42d088
Bela Vlahovics <vlahobgm@gmail.com>
2020-04-14 10:19:29 +0200

    fourth exercise changed with ifPresent

--- week-06/day-01/src/exercises/ExerciseFour.java
+++ week-06/day-01/src/exercises/ExerciseFour.java
@@ -8,11 +8,10 @@ public class ExerciseFour {
   public static void main(String[] args) {
     //Write a Stream Expression to get the average value of the odd numbers from the following list:
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-    OptionalDouble average = numbers.stream()
+    numbers.stream()
         .filter(x -> x % 2 == 1 || x % 2 == -1)
         .mapToInt((x) -> x)
-        .average();
-    System.out.println(average);
-
+        .average()
+        .ifPresent(System.out::println);
   }
 }

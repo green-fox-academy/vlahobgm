6556add50333a0263e05ad7639033611774e2d38
Bela Vlahovics <vlahobgm@gmail.com>
2020-04-16 14:55:10 +0200

    hello user added

--- week-06/day-03/src/main/java/com/greenfoxacademy/springstart/HelloRESTController.java
+++ week-06/day-03/src/main/java/com/greenfoxacademy/springstart/HelloRESTController.java
@@ -3,13 +3,14 @@ package com.greenfoxacademy.springstart;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
   @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-  public Greeting greeting() {
-    return new Greeting(1, "Hello World!");
+  public Greeting greeting(@RequestParam String name) {
+    return new Greeting(1, "Hello " + name);
   }
 }

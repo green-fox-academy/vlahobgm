8f4ebc6c15ee4fef417d5b6ed939282ae87f5bc5
Bela Vlahovics <vlahobgm@gmail.com>
2020-04-16 17:44:41 +0200

    webcounter added

--- week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,15 +1,19 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
+  private AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("counter", counter.incrementAndGet());
     return "greeting";
   }
 

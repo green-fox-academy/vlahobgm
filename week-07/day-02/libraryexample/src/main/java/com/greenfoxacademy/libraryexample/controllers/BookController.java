f0749fa3bb69e98b09565eaf1bc4d06ed82717c5
Bela Vlahovics <vlahobgm@gmail.com>
2020-04-20 20:31:38 +0200

    homework exercise added

---
+++ week-07/day-02/libraryexample/src/main/java/com/greenfoxacademy/libraryexample/controllers/BookController.java
@@ -0,0 +1,75 @@
+package com.greenfoxacademy.libraryexample.controllers;
+
+import com.greenfoxacademy.libraryexample.models.Book;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookController {
+  private List<Book> books = new ArrayList<>();
+
+  public BookController() {
+    books.add(new Book("Cat's Cradle", "Kurt Vonnegut", 1963));
+    books.add(new Book("Do Androids Dream of Electric Sheep?", "Philip K. Dick", 1968));
+  }
+
+  private List<Book> filterBooksByAuthor(String author) {
+    return books.stream()
+        .filter(book -> book.getAuthor().equals(author))
+        .collect(Collectors.toList());
+  }
+
+  @RequestMapping(path = "/books", method = RequestMethod.GET)
+  public String showBooks(Model model, @RequestParam(name = "author", required = false) String author) {
+
+    List<Book> queriedBooks;
+
+    if (author != null) {
+      queriedBooks = filterBooksByAuthor(author);
+    } else {
+      queriedBooks = books;
+    }
+
+    model.addAttribute("books", queriedBooks);
+    return "index";
+  }
+
+  @RequestMapping(path = "/books/{id}/details", method = RequestMethod.GET)
+  public String getBookById(Model model, @PathVariable(name="id") Integer id) {
+    Book bookById = null;
+
+    for(Book book : books) {
+      if (book.getId() == id) {
+        bookById = book;
+      }
+    }
+
+    if (bookById != null) {
+      model.addAttribute("book", bookById);
+    } else {
+      model.addAttribute("error", "No book found");
+    }
+
+    return "details";
+  }
+
+  @RequestMapping(path = "/books/add", method = RequestMethod.GET)
+  public String addBookForm(Model model, @ModelAttribute(name="book") Book book) {
+    model.addAttribute("book", book);
+    return "create";
+  }
+
+  @RequestMapping(path = "/books/add", method = RequestMethod.POST)
+  public String addBook(@ModelAttribute(name="book") Book book) {
+    books.add(book);
+    return "redirect:/books";
+  }
+}
---
+++ week-07/day-02/libraryexample/src/test/java/com/greenfoxacademy/libraryexample/LibraryexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.libraryexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryexampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

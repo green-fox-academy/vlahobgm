935e1a2644ed1ad6b2d818ddee3b4cf1dc873b88
Bela Vlahovics <vlahobgm@gmail.com>
2020-04-28 09:32:51 +0200

    sql exercise added

--- week-08/day-01/todosql/src/main/java/com/greenfoxacademy/todosql/controllers/TodoController.java
+++ week-08/day-01/todosql/src/main/java/com/greenfoxacademy/todosql/controllers/TodoController.java
@@ -5,10 +5,12 @@ import com.greenfoxacademy.todosql.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/todo")
@@ -22,15 +24,26 @@ public class TodoController {
   }
 
   @RequestMapping(path = {"", "/list"}, method = RequestMethod.GET)
-  public String list(Model model){
-    model.addAttribute("todos", todoService.getTodos());
-    model.addAttribute("todo", new Todo());
+  public String list(Model model, @RequestParam(required = false) Boolean isActive){
+    if (isActive == null) {
+      model.addAttribute("todos", todoService.getTodos());
+    }else {
+      model.addAttribute("todos", todoService.isActive(isActive));
+    }
     return "todolist";
   }
 
-  @PostMapping("/register")
-  public String addNewTodo(@ModelAttribute Todo todo, Model model) {
+  @GetMapping(value = "/add")
+  public String addNewTodo(Model model) {
+    model.addAttribute("newTodo",new Todo());
+    return "addtodo";
+  }
+
+  @PostMapping(value = "/add")
+  public String getAddNewTodo(@ModelAttribute Todo todo, Model model) {
     todoService.addTodo(todo);
     return "redirect:/todo/list";
   }
+
+
 }
--- week-08/day-01/todosql/src/main/java/com/greenfoxacademy/todosql/services/TodoService.java
+++ week-08/day-01/todosql/src/main/java/com/greenfoxacademy/todosql/services/TodoService.java
@@ -4,6 +4,7 @@ import com.greenfoxacademy.todosql.models.Todo;
 import com.greenfoxacademy.todosql.repositories.TodoRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +29,21 @@ public class TodoService {
 
   public Iterable<Todo> getTodos() {
     return todoRepository.findAll();
+  }
+
+  public List<Todo> isActive(Boolean isActive) {
+    List<Todo> returnList = new ArrayList<>();
+      todoRepository.findAll()
+          .forEach(returnList::add);
+      if (isActive){
+        return returnList.stream()
+            .filter(todo -> !todo.isDone())
+            .collect(Collectors.toList());
+      } else {
+        return returnList.stream()
+            .filter(todo -> todo.isDone())
+            .collect(Collectors.toList());
+      }
+
   }
 }

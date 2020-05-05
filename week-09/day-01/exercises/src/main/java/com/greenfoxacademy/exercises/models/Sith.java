6d732d590d73ffe14203c647b27ad30604cd5f1e
Bela Vlahovics <vlahobgm@gmail.com>
2020-05-05 16:13:18 +0200

    sith - uppercase added

--- week-09/day-01/exercises/src/main/java/com/greenfoxacademy/exercises/models/Sith.java
+++ week-09/day-01/exercises/src/main/java/com/greenfoxacademy/exercises/models/Sith.java
@@ -34,13 +34,20 @@ public class Sith {
   public void reorderText() {
     List<String> wordsOfText = new ArrayList<>();
     for (String word : this.text.split(" ")) {
-      wordsOfText.add(word);
+      wordsOfText.add(word.toLowerCase());
     }
-    for (int i = 0; i < wordsOfText.size(); i+=2) {
-      Collections.swap(wordsOfText,i,i+1);
+    if (wordsOfText.size() % 2 == 0){
+      for (int i = 0; i < wordsOfText.size(); i += 2) {
+        Collections.swap(wordsOfText, i, i + 1);
+      }
+    } else {
+      for (int i = 0; i < wordsOfText.size()-1; i+=2) {
+        Collections.swap(wordsOfText, i, i + 1);
+      }
     }
     wordsOfText.add("Arrgh. Uhmm. Err..err.err.");
     this.sithText = wordsOfText.stream()
                                   .collect(Collectors.joining(" "));
+    this.sithText = this.sithText.substring(0,1).toUpperCase() + this.sithText.substring(1);
   }
 }
